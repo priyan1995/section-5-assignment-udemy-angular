e1c8d6a0cfde646797664a06a0c25642f12e620e
priyan <45042030+priyan1995@users.noreply.github.com>
2021-01-11 00:59:07 +0530

    section 5 assignment completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  odd: number[] =[];
+  even: number[] =[];
+
+
+  onIntervalFired( firedNumber: number ){
+   if ( firedNumber % 2 === 0 ){
+     this.even.push(firedNumber);
+   }else{
+     this.odd.push(firedNumber);
+   }
+  }
+
 }
+
--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameControlComponent implements OnInit {
 
+  @Output() intervalFired = new EventEmitter<number>();
+  interval;
+  beginNumber = 0;
+
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+  }
+
+  startGame(){
+    this.interval =  setInterval(() => {
+      this.intervalFired.emit(this.beginNumber + 1);
+      this.beginNumber++;
+    }, 1000);
+  }
+
+  pauseGame(){
+    clearInterval(this.interval);
   }
 
 }
